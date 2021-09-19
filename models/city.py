945de3e18d1ee59c8dcb89456706f4138d5093c0
Manuel-condori <2828@holbertonschool.com>
2021-09-18 22:39:24 -0500

    add content task 6

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the city class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """class state inherith from Base"""
+    __tablename__ = 'cities'
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+        places = relationship("Place", cascade="all, delete", backref="cities")
+
+    else:
+        name = ""
+        state_id = ""
